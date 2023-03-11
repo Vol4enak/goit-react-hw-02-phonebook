4b87eaef9c07616ea1fb4c2e230ab05ff245b2e4
Vol4enak <52600927+Vol4enak@users.noreply.github.com>
2023-03-11 17:18:23 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+
 import { nanoid } from 'nanoid';
 import { Container } from './App.styled';
 import { ContactForm } from './ContantForm/contactForm';
